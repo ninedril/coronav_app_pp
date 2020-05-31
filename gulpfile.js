6165ae6ecb4c0af8ab69e146181f80b4cd33eaa1
ninedril@3830T <papiyou.oguper.3830T@gmail.com>
2020-06-01 02:30:14 +0900

    [init] 一旦完成

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'); // gulpの読み込み
+var browserSync = require('browser-sync'); // browser-syncの読み込み
+
+gulp.task('browser-sync', function(done){
+    browserSync.init({
+        server : {
+            baseDir : './src', // 対象となるディレクトリ
+            index : 'index.html', // リロードするHTMLファイル
+        },
+    });
+    done();
+});
+
+gulp.task('reload', function(done){
+    browserSync.reload();
+    done();
+});
+
+// ファイル監視
+gulp.task('watch', gulp.series(['browser-sync'], function(done){
+    gulp.watch('./src/*.+(html|js)', gulp.series(['reload']));
+    done();
+}));
+
+gulp.task('default', gulp.series(['browser-sync', 'watch']));
